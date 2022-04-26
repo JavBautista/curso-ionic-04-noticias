271f717b246c441737225df799773bc9180b00c0
Luis Javier Bautista <jav.bautista@gmail.com>
2022-04-26 10:46:52 -0500

    Inicio de proyecto

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,4 @@
 export const environment = {
-  production: true
+  production: true,
+  apiKey:'43e217f5a4e24208ba416a685c0b9a2f'
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,8 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  apiKey:'43e217f5a4e24208ba416a685c0b9a2f'
 };
 
 /*
